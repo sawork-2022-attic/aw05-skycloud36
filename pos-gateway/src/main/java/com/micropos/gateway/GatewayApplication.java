dc54113c0b06cc1a52efe0ff28ce7de34f1774a4
skycloud36 <1076626296@qq.com>
2022-05-25 10:14:04 +0800

    eureka 1.0

---
+++ pos-gateway/src/main/java/com/micropos/gateway/GatewayApplication.java
@@ -0,0 +1,12 @@
+package com.micropos.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+//@EnableEurekaClient
+public class GatewayApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayApplication.class, args);
+    }
+}
